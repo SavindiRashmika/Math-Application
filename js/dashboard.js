88f02dd65a20e7fb0027777332da429e1e2bedc2
SavindiRashmika <rashmikawadanambi>
2023-11-15 23:14:01 +0530

    single page added

---
+++ js/dashboard.js
@@ -0,0 +1,63 @@
+
+
+let add = document.querySelector("#addition");
+let subtract = document.querySelector("#subtraction");
+let multiply = document.querySelector("#multiply");
+let divide = document.querySelector("#divide");
+let dash = document.querySelector("#dashboard");
+let head = document.querySelector("#container");
+
+add.style.display ='none';
+subtract.style.display ='none';
+multiply.style.display ='none';
+divide.style.display ='none';
+dash.style.display = 'block';
+
+let lnkStart = document.querySelector("#start_button");
+let lnkAdd = document.querySelector("#current");
+let lnkSubtract = document.querySelector("#current1");
+let lnkMultiply =document.querySelector("#current2");
+let lnkDivide =document.querySelector("#current3");
+
+
+lnkStart.addEventListener("click",function (){
+    add.style.display ='block';
+    subtract.style.display ='none';
+    multiply.style.display ='none';
+    divide.style.display ='none';
+    dash.style.display = 'none';
+})
+
+lnkAdd.addEventListener("click",function (){
+    add.style.display ='block';
+    subtract.style.display ='none';
+    multiply.style.display ='none';
+    divide.style.display ='none';
+    dash.style.display = 'none';
+})
+
+lnkSubtract.addEventListener("click",function (){
+    add.style.display ='none';
+    subtract.style.display ='block';
+    multiply.style.display ='none';
+    divide.style.display ='none';
+    dash.style.display = 'none';
+
+})
+
+lnkMultiply.addEventListener("click",function (){
+    add.style.display ='none';
+    subtract.style.display ='none';
+    multiply.style.display ='block';
+    divide.style.display ='none';
+    dash.style.display = 'none';
+})
+
+lnkDivide.addEventListener("click",function (){
+    add.style.display ='none';
+    subtract.style.display ='none';
+    multiply.style.display ='none';
+    divide.style.display ='block';
+    dash.style.display = 'none';
+})
+
