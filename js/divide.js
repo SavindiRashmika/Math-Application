0feb545d1dee3793b5a707a669b44dc1cdcff0b8
SavindiRashmika <rashmikawadanambi>
2023-12-28 21:02:49 +0530

    updated

--- js/divide.js
+++ js/divide.js
@@ -6,14 +6,14 @@ const d_option3 = document.getElementById("d_option3");
 var d_answer = 0;
 
 function Division(){
-    var d_num1 = Math.floor(Math.random() * 20);
+    var d_num1 = Math.floor(Math.random() * 15);
     var d_num2 = Math.floor(Math.random() * 10);
-    var d_dummyAnswer1 = Math.floor(Math.random() * 20);
-    var d_dummyAnswer2 = Math.floor(Math.random() * 10);
+    var d_dummyAnswer1 = Math.floor(Math.random() * 15);
+    var d_dummyAnswer2 = Math.floor(Math.random() * 15);
     var d_allAnswer = [];
     var d_switchAnswer = [];
 
-    d_answer = d_num1 / d_num2;
+    d_answer = (d_num1 / d_num2).toFixed(1);
 
     document.getElementById("d_num1").innerHTML = d_num1;
     document.getElementById("d_num2").innerHTML = d_num2;
@@ -30,7 +30,7 @@ function Division(){
 }
 
 d_option1.addEventListener("click", function (){
-    if (d_option1.innerHTML == d_answer){
+    if (parseFloat(d_option1.innerHTML) == d_answer){
         Division();
     }else {
         audio.play();
@@ -38,7 +38,7 @@ d_option1.addEventListener("click", function (){
 });
 
 d_option2.addEventListener("click", function (){
-    if (d_option2.innerHTML == d_answer){
+    if (parseFloat(d_option2.innerHTML) == d_answer){
         Division();
     }else {
         audio.play();
@@ -46,7 +46,7 @@ d_option2.addEventListener("click", function (){
 });
 
 d_option3.addEventListener("click", function (){
-    if (d_option3.innerHTML == d_answer){
+    if (parseFloat(d_option3.innerHTML) == d_answer){
         Division();
     }else {
         audio.play();
